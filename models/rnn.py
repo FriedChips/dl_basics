676c3f6276422a8e5051819dffcca661c177ef6c
Markus Frank <markus@friedchips.de>
2023-04-06 18:16:35 +0200

    changed gru_01

--- models/rnn.py
+++ models/rnn.py
@@ -1,5 +1,5 @@
 from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import Embedding, LSTM, GRU, Dense, Flatten, Softmax, Multiply
+from tensorflow.keras.layers import Embedding, LSTM, GRU, Dense, Flatten
 from tensorflow.keras.regularizers import L1, L2
 
 
@@ -8,6 +8,7 @@ def build_model_gru_01(
         num_outputs,
         gru_units,
         num_hidden_gru = 0,
+        return_final_hidden = False,
         use_embedding = False,
         vocab_size = None,
         embedding_dim = None,
@@ -19,7 +20,9 @@ def build_model_gru_01(
         x = Embedding(vocab_size, embedding_dim, input_length=input_shape[0], name="embedding")(x)
     for i in range(num_hidden_gru):
         x = GRU(gru_units, return_sequences=True, name=f"gru_{i}")(x)
-    x = GRU(gru_units, name="gru_final")(x)
+    x = GRU(gru_units, return_sequences=return_final_hidden, name="gru_final")(x)
+    if return_final_hidden:
+        x = Flatten(name="flatten")(x)
     x = Dense(num_outputs, name="linear_final")(x)
 
     out = x
