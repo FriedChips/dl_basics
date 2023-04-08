b48cfa19b7d1ef566f27458ab80512a7b00a00a6
Markus Frank <markus@friedchips.de>
2023-04-08 10:17:48 +0200

    SynthMathData: changed uniform range, added minus

--- data_gen/synth_sequences.py
+++ data_gen/synth_sequences.py
@@ -9,14 +9,15 @@ class SynthMathData(tf.keras.utils.Sequence):
         - a is a uniform random number in the range 0...1
         - b is zero, except for 2 randomly chosen features in the first half of the sequence where it is 1
     - target y is
-        - average (a1 + a2)/2 if operation=="Add", where a1 and a2 are from the features where b==1
-        - a1 * a2 if operation=="Multiply"
+        - "renormalized" sum (a1 + a2)/2 if operation=="plus", where a1 and a2 are from the features where b==1
+        - "renormalized" difference (a1 - a2)/2 if operation=="minus"
+        - a1 * a2 if operation=="multiply"
     - this is very similar to Experiments 4 and 5 from the 1997 Hochreiter/Schmidhuber LSTM paper
     """
 
     def __init__(self, batch_size, seq_len, operation, distribution, seed=42):
 
-        assert operation in ["add", "multiply"]
+        assert operation in ["plus", "minus", "multiply"]
         assert distribution in ["uniform", "normal"]
 
         self.batch_size = batch_size
@@ -36,15 +37,17 @@ class SynthMathData(tf.keras.utils.Sequence):
         x_batch, y_batch = [], []
         for _ in range(self.batch_size):
             if self.distribution == "uniform":
-                upper = self.rng.uniform(0, 1, self.seq_len)
+                upper = self.rng.uniform(-1, 1, self.seq_len)
             elif self.distribution == "normal":
                 upper = self.rng.normal(0, 1, self.seq_len)
             x = np.transpose(np.stack([ upper, np.zeros(self.seq_len) ]))
             id1, id2 = self.rng.choice(self.seq_len // 2, 2, replace=False)
             x[id1,1], x[id2,1] = 1, 1
             x1, x2 = x[id1,0], x[id2,0]
-            if self.operation == "add":
-                y = (x1 + x2) / 2.0
+            if self.operation == "plus":
+                y = (x1 + x2) / 2.0 # keep result in range -1...1 if distribution=="uniform"
+            elif self.operation == "minus":
+                y = (x1 - x2) / 2.0 # keep result in range -1...1 if distribution=="uniform"
             elif self.operation == "multiply":
                 y = x1 * x2
             x_batch.append(x)
