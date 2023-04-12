2c0887862b28fa9c9a2e126633a0342bd5e889ba
Markus Frank <markus@friedchips.de>
2023-04-12 17:37:14 +0200

    updates analysis modulo, simple arith

--- models/mlp.py
+++ models/mlp.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
-from tensorflow.keras.activations import relu, gelu
 from tensorflow.keras.regularizers import L1, L2
 
 
@@ -9,6 +8,7 @@ def mlp_01(
         input_shape,
         layer_units,
         activations,
+        use_bias=True,
 ):    
     assert isinstance(layer_units, list) and isinstance(activations, list)
     assert len(input_shape) <= 3
@@ -22,13 +22,13 @@ def mlp_01(
 
     for layer, (units, act) in enumerate(zip(layer_units, activations)):
         if (layer == 0) and len(input_shape) > 1:
-            x = Conv2D(units, input_shape[:2], name=f"dense_{layer}")(x)
+            x = Conv2D(units, input_shape[:2], use_bias=use_bias, name=f"dense_{layer}")(x)
             x = Flatten(name="flatten")(x)
         else:
-            x = Dense(units, name=f"dense_{layer}")(x)
-        assert act in ["linear", "relu", "gelu"]
+            x = Dense(units, use_bias=use_bias, name=f"dense_{layer}")(x)
+        #assert act in ["linear", "relu", "gelu"]
         if act != "linear":
-            x = eval(act)(x)
+            x = eval("tf.keras.activations." + act)(x)
 
     out = x
     model = Model(inp, out, name="mlp_01")
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     # building test
     mlp_01(
         input_shape=(28,28),
-        layer_units=[32,16,8],
-        activations=["relu", "linear", "gelu"],
+        layer_units=[32,16,8,4],
+        activations=["relu", "linear", "gelu", "tanh"],
+        use_bias=False,
     ).summary()
